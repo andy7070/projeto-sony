3d13b8f2794d6d592edbfe46930c9f3755661794
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-07-03 05:33:24 -0300

    update
    ta ficando show

--- menu2.js
+++ menu2.js
@@ -4,10 +4,10 @@ const menuItem = document.querySelectorAll('.mover2')
 
 function selectLink(){
     menuItem.forEach((item)=>
-    item.classList.remove('ativo')
+    item.classList.remove('ativo2')
     
     )
-    this.classList.add('ativo')
+    this.classList.add('ativo2')
 }
 
 
@@ -15,9 +15,9 @@ menuItem.forEach((item)=>
    item.addEventListener('click', selectLink)
 )
 
-const exp = document.querySelector('#exp')
-const menu = document.querySelector('.menu-lateral')
+const exp = document.querySelector('#exp2')
+const menu = document.querySelector('.menu-lateral2')
 
 exp.addEventListener('click', function(){
-    menu.classList.toggle('expandir')
+    menu.classList.toggle('expandir2')
 })
